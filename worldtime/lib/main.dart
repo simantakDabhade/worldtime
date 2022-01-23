7c1f913a2ae9ee8049df7f6cb7727c9b6d3d42d0
simantakDabhade <67303107+simantakDabhade@users.noreply.github.com>
2022-01-23 20:05:56 +0800

    initial upload

---
+++ worldtime/lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:worldtime/pages/choose_location.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: "/",
+    routes: {
+      "/": (context) => Loading(),
+      "/home": (context) => Home(),
+      "/location": (context) => ChooseLocation(),
+    },
+  ));
+}
